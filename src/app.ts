7920989422fa01b4e004c3ddb4aa67ec1b47675d
Maxi Ordoñez <exemaxi@outlook.com>
2026-09-06 00:22:07 -0300

    Agregar dependencia morgan

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import categoriaRoutes from "./routes/categoria.routes";
 
 const app = express();
 
 app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
