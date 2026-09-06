8d2b38b1166fbb9b82fc398b77e8d73c36671e55
Maxi Ordoñez <exemaxi@outlook.com>
2026-09-05 21:05:14 -0300

    separar config inicial en app.ts e index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,7 @@
-import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
-import { PrismaClient } from "./generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import app from "./app";
 
 dotenv.config();
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
-
-const app = express();
-const prisma = new PrismaClient({ adapter });
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Servidor funcionando 🚀");
-});
-
-app.get("/productos", async (req, res) => {
-  const productos = await prisma.producto.findMany();
-  res.json(productos);
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
