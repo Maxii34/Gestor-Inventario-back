72e808fcec8507a580fdce8464755535e224c112
Maxi Ordoñez <exemaxi@outlook.com>
2026-09-07 22:58:50 -0300

    Agregar verificarToken en rutas venta, producto, movimiento, cliente, categoria

--- src/routes/cliente.routes.ts
+++ src/routes/cliente.routes.ts
@@ -1,17 +1,15 @@
 import { Router } from "express";
 import { clienteController } from "../controllers/cliente.controller";
 import { validate } from "../middlewares/validate";
-import {
-  crearClienteSchema,
-  actualizarClienteSchema,
-} from "../validators/cliente.validate";
+import { verificarToken } from "../middlewares/auth.middleware";
+import { crearClienteSchema, actualizarClienteSchema } from "../validators/cliente.validate";
 
 const router = Router();
 
-router.get("/", clienteController.getAll);
-router.get("/:id", clienteController.getById);
-router.post("/", validate(crearClienteSchema), clienteController.create);
-router.put("/:id", validate(actualizarClienteSchema), clienteController.update);
-router.delete("/:id", clienteController.delete);
+router.get("/", verificarToken, clienteController.getAll);
+router.get("/:id", verificarToken, clienteController.getById);
+router.post("/",verificarToken, validate(crearClienteSchema), clienteController.create);
+router.put("/:id",verificarToken, validate(actualizarClienteSchema), clienteController.update);
+router.delete("/:id",verificarToken, clienteController.delete);
 
 export default router;
--- src/routes/venta.routes.ts
+++ src/routes/venta.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { ventaController } from "../controllers/venta.controller";
 import { validate } from "../middlewares/validate";
+import { verificarToken } from "../middlewares/auth.middleware";
 import { crearVentaSchema, actualizarVentaSchema } from "../validators/venta.validation";
 
 const router = Router();
 
-router.get("/", ventaController.getAll);
-router.get("/:id", ventaController.getById);
-router.post("/", validate(crearVentaSchema), ventaController.create);
-router.put("/:id", validate(actualizarVentaSchema), ventaController.update);
-router.delete("/:id", ventaController.delete);
+router.get("/", verificarToken, ventaController.getAll);
+router.get("/:id", verificarToken, ventaController.getById);
+router.post("/", verificarToken, validate(crearVentaSchema), ventaController.create);
+router.put("/:id", verificarToken, validate(actualizarVentaSchema), ventaController.update);
+router.delete("/:id", verificarToken, ventaController.delete);
 
 export default router;
